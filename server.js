03d19e87b72045537453e0cf279ccdc3e76c5287
Tarun-kondaveti <tarun.kondaveti@gmail.com>
2025-03-28 15:20:14 +0530

    Problem 1 done

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import axios from "axios";
+
+const app=express();
+const port=9876;
+
+const wsize= 10;
+let nums = [];
+app.get('/nums/:numberId', async (req, res) => {
+  const numberId = req.params.numberId;
+  let newNums = [];
+  try {
+    const resp = await axios.get(`http://20.244.56.144/test/${numberId}`,{ timeout: 500 });
+ newNums = resp.data.nums;
+  } catch (error) {
+    console.error(error);
+  }
+  nums = [...new Set([...nums, ... newNums])];
+  if (nums.length > wsize) {
+    nums = nums.slice(-wsize);
+  }
+  const avg = nums.reduce((a, b) => a + b, 0) / nums.length || 0;
+  res.json({
+    windowPrevState: nums.slice(0,  newNums.length),
+    windowCurrState: nums,
+    nums: newNums,
+    avg
+  });
+});
+app.listen(port, () => {
+  console.log('Server listening on port 9876');
+});
